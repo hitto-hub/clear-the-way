668b19d7a72b88b3936745f56667463de0a5e197
hitto <68594072+hitto-hub@users.noreply.github.com>
2022-10-23 02:05:50 +0900

    add pro

--- komudotto.js
+++ komudotto.js
@@ -1,3 +1,6 @@
+// 変数の宣言
+var not = 0;
+var pro = 0;
 // ボタンdisableの関数、etc...
 function btnDisable(){
     document.getElementById("btn").disabled = true;
@@ -19,8 +22,21 @@ function clicked_ez(){
     statusDis  = setInterval(btnDisable , 1);
     statusAble = setInterval(btnAble , 200);
 }
-// 変数の宣言
-var not = 0;
+// pro
+const func_pro = () => {
+    if (pro == 0) {
+        pro = 1;
+        target = document.getElementById("pro");
+        target.innerHTML = "pro version";
+        console.log(pro);
+    } else {
+        pro = 0;
+        target = document.getElementById("pro");
+        target.innerHTML = "";
+        console.log(pro);
+    }
+}
+
 // ボタンが押された時の処理
 const func_kom = () => {
     function getRandomInt(max) {
@@ -69,12 +85,14 @@ const func_kom = () => {
     target.innerHTML = "";
     if (output == "コムドットが通るから道をあけろ俺らが日本を獲る"){
         // ここにコムドットだった時の処理を書く
-        clicked_ez();
         target = document.getElementById("count");
         target.innerHTML = "おまえがコムドットだ！";
-        const music = new Audio('se/fafa-.wav');
-        music.volume = 0.1;
-        music.play();
+        if (pro == 0) {
+            const music = new Audio('se/fafa-.wav');
+            music.volume = 0.1;
+            music.play();
+            clicked_ez();
+        }
     } else {
         // ここにコムドットじゃなかった時の処理を書く
         var output = document.getElementById("output").innerHTML;
@@ -107,38 +125,48 @@ const func_kom = () => {
     } else if (kakuritu == 0.00152587890625) {
         target = document.getElementById("rare");
         target.innerHTML = "まあまあすごい";
+        if (pro == 0) {
         const music = new Audio('se/kya-yello.wav');
         music.volume = 0.5;
         music.play();
-        clicked_ban()
+            clicked_ban()
+        }
     } else if (kakuritu == 0.0003814697265625) {
         target = document.getElementById("rare");
         target.innerHTML = "すごい";
         const music = new Audio('se/ban1.wav');
         music.volume = 0.5;
         music.play();
-        clicked_ban()
+        if (pro == 0) {
+            clicked_ban()
+        }
     } else if (kakuritu == 0.000095367431640625) {
         target = document.getElementById("rare");
         target.innerHTML = "すごすぎる";
         const music = new Audio('se/ban2.wav');
         music.volume = 0.5;
         music.play();
-        clicked_ban()
+        if (pro == 0) {
+            clicked_ban()
+        }
     } else if (kakuritu == 0.00002384185791015625) {
         target = document.getElementById("rare");
         target.innerHTML = "まじやばい";
         const music = new Audio('se/ban3.wav');
         music.volume = 0.5;
         music.play();
-        clicked_ban()
+        if (pro == 0) {
+            clicked_ban()
+        }
     } else if (kakuritu == 0.0000059604644775390625) {
         target = document.getElementById("rare");
         target.innerHTML = "まじやばすぎる";
         const music = new Audio('se/ban4.wav');
         music.volume = 0.5;
         music.play();
-        clicked_ban()
+        if (pro == 0) {
+            clicked_ban()
+        }
     } else {
         target = document.getElementById("rare");
         target.innerHTML = "暇人＾＾;";

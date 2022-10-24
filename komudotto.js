68d052886fafad5362c59ea1f77917fc7af451bc
hitto <68594072+hitto-hub@users.noreply.github.com>
2022-10-24 21:26:15 +0900

    変数名kakuritu->percent count = output.length;

--- komudotto.js
+++ komudotto.js
@@ -59,33 +59,33 @@ const func_kom = () => {
     var nihonn = "日本"
     var toru = "獲る"
     var output = ""
-    var kakuritu = 100
+    var percent = 100
     output += komudotto
     while (true) {
         output += ga
         randamu = (getRandomInt(2));
         if (randamu >= 1) {
             output += tooru + kara + miti
-            kakuritu = kakuritu / 2
+            percent = percent / 2
         } else {
             output += nihonn
-            kakuritu = kakuritu / 2
+            percent = percent / 2
         }
         output += wo
         randamu = (getRandomInt(2));
         if (randamu >= 1){
             output += toru
-            kakuritu = kakuritu / 2
+            percent = percent / 2
             break
         } else {
             output += akero + orera
-            kakuritu = kakuritu / 2
+            percent = percent / 2
         }
     }
     // 結果
     target = document.getElementById("output");
     target.innerHTML = output;
-    // 文字数
+    // 文字数 ↓果たして下2行は本当に要るのか？
     target = document.getElementById("count");
     target.innerHTML = "";
     if (output == "コムドットが通るから道をあけろ俺らが日本を獲る"){
@@ -103,6 +103,7 @@ const func_kom = () => {
         // ここにコムドットじゃなかった時の処理を書く
         target = document.getElementById("count");
         target.innerHTML =output.length + "文字";
+        count = output.length;
     }
     if (max < output.length){
         max = output.length
@@ -110,25 +111,25 @@ const func_kom = () => {
         target.innerHTML = "最長記録：" + max + "文字";
     }
     // rareの処理
-    if (kakuritu == 25) {
+    if (percent == 25) {
         target = document.getElementById("rare");
         target.innerHTML = "よわい";
-    } else if (kakuritu == 6.25) {
+    } else if (percent == 6.25) {
         target = document.getElementById("rare");
         target.innerHTML = "ふつう";
-    } else if (kakuritu == 1.5625) {
+    } else if (percent == 1.5625) {
         target = document.getElementById("rare");
         target.innerHTML = "ぼちぼち";
-    } else if (kakuritu == 0.390625) {
+    } else if (percent == 0.390625) {
         target = document.getElementById("rare");
         target.innerHTML = "まあまあ";
-    } else if (kakuritu == 0.09765625) {
+    } else if (percent == 0.09765625) {
         target = document.getElementById("rare");
         target.innerHTML = "そこそこ";
-    } else if (kakuritu == 0.0244140625) {
+    } else if (percent == 0.0244140625) {
         target = document.getElementById("rare");
         target.innerHTML = "ちょっとだけすごい";
-    } else if (kakuritu == 0.006103515625) {
+    } else if (percent == 0.006103515625) {
         target = document.getElementById("rare");
         target.innerHTML = "ぼちぼちすごい";
         if (pro == 0) {
@@ -138,7 +139,7 @@ const func_kom = () => {
             clicked_ban()
         }
     // 厚い壁
-    } else if (kakuritu == 0.00152587890625) {
+    } else if (percent == 0.00152587890625) {
         target = document.getElementById("rare");
         target.innerHTML = "まあまあすごい";
         const music = new Audio('se/ban1.wav');
@@ -147,7 +148,7 @@ const func_kom = () => {
         if (pro == 0) {
             clicked_ban()
         }
-    } else if (kakuritu == 0.0003814697265625) {
+    } else if (percent == 0.0003814697265625) {
         target = document.getElementById("rare");
         target.innerHTML = "すごい";
         const music = new Audio('se/ban1.wav');
@@ -156,7 +157,7 @@ const func_kom = () => {
         if (pro == 0) {
             clicked_ban()
         }
-    } else if (kakuritu == 0.000095367431640625) {
+    } else if (percent == 0.000095367431640625) {
         target = document.getElementById("rare");
         target.innerHTML = "すごすぎる";
         const music = new Audio('se/ban2.wav');
@@ -165,14 +166,14 @@ const func_kom = () => {
         if (pro == 0) {
             clicked_ban()
         }
-    } else if (kakuritu == 0.00002384185791015625) {
+    } else if (percent == 0.00002384185791015625) {
         target = document.getElementById("rare");
         target.innerHTML = "まじやばい";
         const music = new Audio('se/ban3.wav');
         music.volume = 0.5;
         music.play();
         clicked_ban()
-    } else if (kakuritu == 0.0000059604644775390625) {
+    } else if (percent == 0.0000059604644775390625) {
         target = document.getElementById("rare");
         target.innerHTML = "まじやばすぎる";
         const music = new Audio('se/ban4.wav');
@@ -188,8 +189,8 @@ const func_kom = () => {
         clicked()
     }
     // 確率を表示する
-    target = document.getElementById("kakuritu");
-    target.innerHTML = kakuritu + "%";
+    target = document.getElementById("percent");
+    target.innerHTML = percent + "%";
     // 何回押したか
     not++;
     document.getElementById("num").innerHTML = not;
